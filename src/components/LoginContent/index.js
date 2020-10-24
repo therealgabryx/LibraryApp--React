2f6b72cdd2935cac592df3322ffdd0d95b5bfa0e
therealgabryx <gabriele.concli@gmail.com>
2020-10-24 07:53:26 +0000

    Signup page UI layouted

--- src/components/LoginContent/index.js
+++ src/components/LoginContent/index.js
@@ -1,10 +1,20 @@
 import React from 'react' 
 import './index.css' 
 
+// components
+import { Form, FormGroup, FormInput } from 'shards-react'
+
 export default function index() { 
     return ( 
         <div className='LoginContent'> 
-            
+            <Form>
+                <FormGroup>
+                    <FormInput/>
+                </FormGroup>
+                <FormGroup>
+                    <FormInput/>
+                </FormGroup>
+            </Form>
         </div> 
     ) 
 } 
--- src/components/SignupContent/index.js
+++ src/components/SignupContent/index.js
@@ -1,9 +1,41 @@
 import React from 'react'
 import './index.css'
 
+// components
+import { Form, FormGroup, FormInput, Button } from 'shards-react'
+import { Link } from 'react-router-dom'
+
 export default function index() {
     return (
-        <div className='SignupContent'>
+        <div className='SignupContent'> 
+            <div className='info'>
+                <h6> 
+                    Sign up by creating a new account or <Link to='/login'>Log in </Link> 
+                    to get access to additional features like saving your favouite books
+                    or displaying books info. 
+                </h6> 
+            </div>
+            <div className='form'>
+                <Form> 
+                    <FormGroup> 
+                        <label htmlFor='#username'>Username</label> 
+                        <FormInput id='username' placeholder='username'/> 
+                    </FormGroup> 
+                    <FormGroup> 
+                        <label htmlFor='#email'>Email</label> 
+                        <FormInput id='email' placeholder='mail@example.com' type='email'/> 
+                    </FormGroup> 
+                    <FormGroup> 
+                        <label htmlFor='#password'>Password</label> 
+                        <FormInput id='password' placeholder='password' type='password'/> 
+                    </FormGroup>
+                </Form> 
+            </div>
+
+            <div className='actions'>
+                <Button>Signup</Button>
+                <Link to='/login'>or Log in here</Link>
+            </div>
             
         </div>
     )
